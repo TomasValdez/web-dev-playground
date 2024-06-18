3d5690d09eb7b4ae2d217f21a427916a09ce81c2
Alberto Valdez <L16091228@zacatepec.tecnm.mx>
2024-06-18 09:24:34 -0600

    control z

---
+++ php/index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Web Site </title>
+    <link rel="stylesheet" href="web-dev-playground/css/style.css">
+</head>s
+<body>
+    <header> 
+        <div class="container__header">
+            <div class="logo">
+                <a href="#">
+                    <img src="web-dev-playground/images/Logo/logo-magtimus.png" alt="">
+                </a>
+            </div>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>   
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Servicios</a></li>
+                        <li><a href="#">Obras</a></li>
+                        <li><a href="#">Contactos</a></li>
+                    </ul>
+
+                    <a href="#" class="btn__quote">Cotizar</a>
+
+                    <div class="socialMedia">
+                        <a href="#">
+                            <img src="web-dev-playground/images/socialmedia/facebook.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="web-dev-playground/images/socialmedia/instagram.png" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="web-dev-playground/images/socialmedia/twitter.png alt=">
+                        </a>
+                        <a href="#">
+                            <img src="web-dev-playground/images/socialmedia/youtube.png" alt="">
+                        </a>
+                </nav>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
